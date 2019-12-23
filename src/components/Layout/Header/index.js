c639b2197400acacd12beeb70deb12dd94bbdd9e
Raphael Costa <raphacm@gmail.com>
2019-12-23 20:25:50 -0300

    create Header component

---
+++ src/components/Layout/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__content">
+        <h1>My Store</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
