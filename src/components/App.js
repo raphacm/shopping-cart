ee7dabc6e006bd03d66aecac964f44c28c8da8fb
Raphael Costa <raphacm@gmail.com>
2019-12-27 02:13:23 -0300

    render list of products

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import withMainLayout from "./Layout/MainLayout/withMainLayout";
+import List from "./Layout/List";
+import ProductItem from "./Products/ProductItem";
+import gameList from "../products.json";
+
+const ListWithMainLayout = withMainLayout(List);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ListWithMainLayout
+        items={gameList}
+        renderComponent={item => <ProductItem key={item.id} {...item} />}
+      />
+    </>
+  );
 }
 
 export default App;
